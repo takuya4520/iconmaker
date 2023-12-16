ad285b40f7c23b461a0e3b52f9e614b848667f25
takuya <108344446+takuya4520@users.noreply.github.com>
2023-12-16 11:51:24 +0900

    アプリの大枠の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  require "openai"
+
+  before_action :set_common_variable
+
+  private
+
+  # APIキーを設定
+  def set_common_variable
+    api_key = Rails.application.credentials[:openai][:api_key]
+    @client = OpenAI::Client.new(access_token: api_key)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
-
-  resources :chatbots
-
-  root "chatbots#index"
-  
+  root to: 'chats#index'
+  resources :chats
+  get 'search', to: 'chats#search'
 end
