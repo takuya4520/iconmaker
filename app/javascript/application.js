6ca839fcf8bf4c9c66d8f80ad2b0129ff51a8218
takuya <108344446+takuya4520@users.noreply.github.com>
2024-01-03 16:39:57 +0900

    読み込み中の画像追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$(window).on('load', function () {
+  $("#loading").delay(1500).fadeOut('slow'); // ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+  $("#loading_box").delay(1500).fadeOut('slow'); // ローディング画像を1.2秒（1200ms）待機してからフェードアウト
+});
