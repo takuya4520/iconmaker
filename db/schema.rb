f8d8e39da3ab7630d3aa064569f47c080a0e14a7
takuya <108344446+takuya4520@users.noreply.github.com>
2024-01-06 18:24:04 +0900

    sorceryの初期設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_17_051502) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_06_091711) do
   create_table "queries", force: :cascade do |t|
     t.string "query", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "stories", force: :cascade do |t|
-    t.string "plan"
-    t.text "content"
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
