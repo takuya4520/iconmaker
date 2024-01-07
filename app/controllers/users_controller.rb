2527cf9031e14b2eb2e9ba67e75d9e169aa79597
takuya <108344446+takuya4520@users.noreply.github.com>
2024-01-07 10:58:42 +0900

    ユーザー登録ログイン機能実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # POST /users 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :salt, :name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'login', to: 'user_sessions#new' #login_pathが使えるようになり、login_pathが使われるとログイン画面（'user_sessions#new'）に移動する
+  post 'login', to: 'user_sessions#create' #login_path('user_sessions#new')からのデータを'user_sessions#create'へ受け取って、データベースに保存
+  delete 'logout', to: 'user_sessions#destroy' #logout_pathが使えるようになる。削除処理を行う。
+
+  resources :users, only: %i[new create]
   # Defines the root path route ("/")
   root to: 'images#top'
   get 'create_icon', to: 'images#create_image'
